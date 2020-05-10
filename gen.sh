0f6142c932e4f3bacae9ec3f39a68f31ab4d8909
josh <joshbothun@gmail.com>
2020-05-09 21:08:43 -0700

    Add the ability to override puml-gen env vars

--- gen.sh
+++ gen.sh
@@ -4,5 +4,4 @@ RELATIVE_INPUT_DIR=$1
 RELATIVE_OUTPUT_DIR=$2
 
 puml-gen -dir $PUML_GEN_ARGS /code/$RELATIVE_INPUT_DIR /code/$RELATIVE_OUTPUT_DIR
-# PlantUML expects .pu files but puml-gen writes .puml for some reason.
 java -jar plantuml.jar /code/$RELATIVE_OUTPUT_DIR/**.puml
